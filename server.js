65e14f1167f26b93c8f50fe5220bd9d2605b9736
cScholze <caleb.scholze@gmail.com>
2016-03-01 13:19:35 -0600

    added jade view engine and homepage

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 'use strict';
 
+// Modules and dependencies
 const express = require('express');
+const path = require('path');
 
+
+// Data Models
 const models = require('./models/');
 
+// App variables
 const PORT = process.env.PORT || 3000;
 
+
+// Initialize express app
 const app = express();
 
+
+// Middleware
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Root
 app.get('/', (req, res) => {
-  res.send({
-    status: 'Success'
-  });
+  res.status(200).render('home');
 });
 
 
@@ -25,19 +36,19 @@ app.get('/genres', (req, res) => {
 // MediaTypes
 app.get('/mediatypes', (req, res) => {
   models.MediaType.findAll()
-    .then(types => res.send(types));
+    .then( types => res.send(types) );
 });
 
 // Artists
 app.get('/artists', (req, res) => {
   models.Artist.findAll()
-    .then(artists => res.send(artists));
+    .then( artists => res.send(artists) );
 });
 
 // Playlists
 app.get('/playlists', (req, res) => {
   models.Playlist.findAll()
-    .then(playlists => res.send(playlists));
+    .then( playlists => res.send(playlists) );
 });
 
 // Albums
@@ -49,9 +60,54 @@ app.get('/albums', (req, res) => {
         attributes: ['Name']
       }
     })
-    .then(albums=> res.send(albums));
+    .then( albums => res.send(albums) );
 });
 
+
+// Invoices
+app.get('/invoices', (req, res) => {
+  models.Invoice.findAll({
+    attributes: {exclude: 'CustomerId' },
+    include: {
+      model: models.Customer,
+      attributes: {exclude: [
+        'CustomerId',
+        'SupportRepId'
+      ]}
+    }
+  })
+    .then( invoices => res.send(invoices) );
+});
+
+// All Customers
+app.get('/customers', (req, res) => {
+  models.Customer.findAll()
+     .then( customers => res.send(customers) );
+});
+
+// Single Customer
+app.get('/customers/:id', (req, res) => {
+  models.Customer.findOne({
+    where: {
+      CustomerId: req.params.id
+    }
+  }).then( customer => res.send(customer));
+});
+
+// One Customer Invoices
+app.get('/customers/:id/invoices', (req, res) => {
+  models.Invoice.findAll({
+    where: {
+      CustomerId: req.params.id
+    },
+    include: {
+      model: models.Customer
+    }
+  })
+  .then(invoices => res.send(invoices));
+});
+
+// Start server listening
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
